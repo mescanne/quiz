6b47a31f786f743ea9ba022278a0872b87ecc1c9
Mark Scannell <scannell@google.com>
2020-10-15 18:58:09 +0100

    updated

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -5,22 +5,22 @@
 <script>
 
 import * as firebase from 'firebase/app';
-import * as firebaseui from 'firebaseui'
+//import * as firebaseui from 'firebaseui'
 
 export default {
   name: 'auth',
   mounted() {
     console.log('firebase:', firebase);
-    var uiConfig = {
-      signInSuccessUrl: '/success',
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ]
-    };
-    console.log('firebaseui:', firebaseui);
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start('#firebaseui-auth-container', uiConfig);
+    // var uiConfig = {
+    //   signInSuccessUrl: '/success',
+    //   signInOptions: [
+    //     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //     firebase.auth.EmailAuthProvider.PROVIDER_ID
+    //   ]
+    // };
+    // console.log('firebaseui:', firebaseui);
+    // var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // ui.start('#firebaseui-auth-container', uiConfig);
   },
 }
 </script>
