a5898fd3d997f072f6f140a34a45b866af00e495
Mark Scannell <mescanne@gmail.com>
2021-05-19 18:09:16 +0100

    Added distribution directory to Git

---
+++ dist/precache-manifest.4de64743cf11168308eedfafafc9996b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a72f3d2dc90f8008b9",
+    "url": "/css/app.89749a7d.css"
+  },
+  {
+    "revision": "040f9e8a96c750383ba6",
+    "url": "/css/chunk-vendors.3ef6ec65.css"
+  },
+  {
+    "revision": "256f2b4d4d4be7c6d6a3cc5c4f1a2877",
+    "url": "/disabled.json"
+  },
+  {
+    "revision": "2c5c5fe57fce7ffa4a89dc107b208d20",
+    "url": "/img/KidsQuizLogo.png"
+  },
+  {
+    "revision": "c2097b3e2ade01ede41a34f2bc52386f",
+    "url": "/img/KidsQuizLogo.svg"
+  },
+  {
+    "revision": "f862c08e6832b8758703d8662844595a",
+    "url": "/img/KidsQuizTitle.svg"
+  },
+  {
+    "revision": "e8c58a59add31254ac251a726cea7278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a72f3d2dc90f8008b9",
+    "url": "/js/app.d5de18ee.js"
+  },
+  {
+    "revision": "040f9e8a96c750383ba6",
+    "url": "/js/chunk-vendors.033f3bb2.js"
+  },
+  {
+    "revision": "9c7dcb5bd80cf0ae7140b0a4fb8d700c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
